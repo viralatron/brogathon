248efa64a59aede579249df25043e3c35601ec25
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2021-09-16 13:26:22 -0300

    get words check and display typing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import wordList from './resources/words.json';
+import Word from "./Word";
+
+const CHAR_LIMIT = 47;
+
+const getWord = () => {
+    const index = Math.floor(Math.random() * wordList.length);
+
+    const word = wordList[index];
+
+    return word.toLowerCase();
+}
 const App = () => {
+    const [typedKeys, setTypedKeys] = useState([]);
+    const [validKeys, setValidKeys] = useState([]);
+    const [word, setWord] = useState('');
+
+    const isValidKey = (key, word) => {
+        if(!word || !key) return false;
+        const result = word.split('').includes(key);
+
+        return result;
+    };
+
+    const handleKeyDown = (e) => {
+        e.preventDefault();
+    
+        const { key } = e;
+
+        setTypedKeys((prev) => [...prev, key].slice(-1 * CHAR_LIMIT));
+
+        if(isValidKey(key, word)){
+            
+            setValidKeys((prev) => {
+                const isValidLenth = prev.length <= word.length;
+                const isNextChar = isValidLenth && word[prev.length] == key;
+                
+                return (isNextChar) ? [...prev, key] : prev;
+            });
+        }
+        
+        console.log('key', key);
+    };
+
+    useEffect(() => {
+        setWord(getWord());
+    }, []);
+
     return(
-        <div className="container">
+        <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
             <div className="valid-keys">
-                <span className="matched">dan</span>
-                <span className="remainder">iel</span>
+                <Word word={word} validKeys={validKeys} />
             </div>
-            <div className="typed-keys">3tudiohudiuy193761298367</div>
+            <div className="typed-keys">{ typedKeys ? typedKeys.join(' ') : null}</div>
             <div className="completed-words">
                 <ol>
                     <li>alface</li>
---
+++ src/Word.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+
+const Word = ({ word, validKeys } ) => {
+    if(!word) return null;
+    const joinedKeys = validKeys.join('');
+
+    const matched = word.slice(0, joinedKeys.length);
+
+    const remainder = word.slice(joinedKeys.length);
+
+    return (
+        <>
+            <span className="matched">{ matched }</span>
+            <span className="remainder">{ remainder }</span>
+        </>
+    )
+}
+
+export default Word
